5a9fbc44e516125422937a26deb586261348830f
rutamt <95180713+rutamt@users.noreply.github.com>
2024-07-06 15:45:29 -0400

    Creation of project: added styling and project
    Co-authored-by: Jaewon <keifnao@users.noreply.github.com>

--- src/App.js
+++ src/App.js
@@ -1,23 +1,56 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { useEffect, useState } from "react";
+import { Button, Flex, Segmented } from "antd";
+
+let nextId = 0;
 
 function App() {
+  const [quote, setQuote] = useState("");
+  const [authors, setAuthors] = useState([]);
+
+  const boxStyle = {
+    width: "100%",
+    height: 120,
+  };
+
+  useEffect(() => {
+    fetch("https://api.quotable.io/quotes/random?limit=4")
+      .then((response) => response.json())
+      .then((data) => {
+        // Choose one random quote
+        const randomQuote = data[Math.floor(Math.random() * data.length)];
+        setQuote(randomQuote.content);
+
+        data.forEach((quote) => {
+          setAuthors([...authors, { id: nextId++, name: quote.author }]);
+          console.log(quote.author, "quote");
+          console.log(authors, "author");
+        });
+      });
+  }, []);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="quote-quest">
+        <div className="quote-wrapper">
+          <div className="quote">{quote}</div>
+        </div>
+
+        <div className="answer">
+          <Button type="primary" className="btn">
+            Primary
+          </Button>
+          <Button type="primary" className="btn">
+            Primary
+          </Button>
+          <Button type="primary" className="btn">
+            Primary
+          </Button>
+          <Button type="primary" className="btn">
+            Primary
+          </Button>
+        </div>
+      </div>
     </div>
   );
 }
